511140a1d1dcc736acf8041cdde604ef32b9e0cc
Jan Kieseler <jkiesele@cern.ch>
2025-08-19 09:13:48 +0200

    thread safe versions

--- MonitoredPump.h
+++ MonitoredPump.h
@@ -10,6 +10,7 @@ extern "C" {
 #include "esp_task_wdt.h"
 #include "LoggingBase.h"
 #include <atomic>
+#include "threadSafeArduino.h"
 
 
 
@@ -133,7 +134,7 @@ public:
     bool runForMl(float ml, bool fulldiagnostics=false) override;
 
     void stop() override {
-        digitalWrite(enablePin_, LOW);
+        threadSafe::digitalWrite(enablePin_, LOW);
     }
 
     const PumpDiagnostics& getDiagnostics() const {
@@ -157,7 +158,7 @@ bool MonitoredPump<Lookahead>::runForPulses(uint32_t pulses, bool fulldiagnostic
     //set up the diagnostics
     //read the baseline
     diagnostics_.clear();
-    diagnostics_.baseline = touchRead(touchPin_);
+    diagnostics_.baseline = threadSafe::touchRead(touchPin_);
     if(capBaseline_ == 0){
         capBaseline_ = diagnostics_.baseline;
     }
@@ -176,16 +177,16 @@ bool MonitoredPump<Lookahead>::runForPulses(uint32_t pulses, bool fulldiagnostic
     }
     unsigned long totalSamples = 0;
     //run the pump
-    digitalWrite(enablePin_, HIGH);
+    threadSafe::digitalWrite(enablePin_, HIGH);
     const unsigned long intervalMs = 2;
     float raw_average = 0.0;
     while(pulses > 0){
         if (abortFlag && abortFlag->load(std::memory_order_relaxed)) {
-            digitalWrite(enablePin_, LOW);
+            threadSafe::digitalWrite(enablePin_, LOW);
             return false;                // aborted early
         }
         //read the current value
-        unsigned long raw_value = touchRead(touchPin_);
+        unsigned long raw_value = threadSafe::touchRead(touchPin_);
         raw_average += raw_value;
         int32_t value = raw_value - capBaseline_;//can subtract directly
         //add the sample to the detectors, raw here as it needs to be >0
@@ -227,7 +228,7 @@ bool MonitoredPump<Lookahead>::runForPulses(uint32_t pulses, bool fulldiagnostic
     raw_average /= totalSamples;
     capBaseline_ = raw_average;
     //stop the pump
-    digitalWrite(enablePin_, LOW);
+    threadSafe::digitalWrite(enablePin_, LOW);
     //return the diagnostics
     return true;
 }
